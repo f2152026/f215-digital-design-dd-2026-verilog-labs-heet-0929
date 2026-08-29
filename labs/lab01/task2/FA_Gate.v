3f4dc1295f9e5cac65832a0cf70c9a7e29038d37
Heet Jain <f20240929@pilani.bits-pilani.ac.in>
2026-08-29 12:15:25 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -27,9 +27,9 @@ module FA_Gate(
   wire ps, pc1, pc2;
 
   xor #(2,3) (ps,  a,   b);
-  and #(2,1) (pc1, a,   b);
-  xor #(2,5) (sum, cin, ps);
-  and #(2,2) (pc2, cin, ps);
-  or  #(2,4) (cout, pc1, pc2);
+  and #(2,3) (pc1, a,   b);
+  xor #(2,3) (sum, cin, ps);
+  and #(2,3) (pc2, cin, ps);
+  or  #(2,3) (cout, pc1, pc2);
 
 endmodule
