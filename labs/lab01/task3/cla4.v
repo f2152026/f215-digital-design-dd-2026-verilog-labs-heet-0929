7962ce8d94eeb6cd020f1d0ee31d56f1b572b8e8
Heet Jain <f20240929@pilani.bits-pilani.ac.in>
2026-08-29 12:05:21 +0000

    lab01 task3

--- labs/lab01/task3/cla4.v
+++ labs/lab01/task3/cla4.v
@@ -34,4 +34,60 @@ module cla4(
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
 
+
+  // Generate and propagate
+  xor #(2) (p0, a[0], b[0]);
+  xor #(2) (p1, a[1], b[1]);
+  xor #(2) (p2, a[2], b[2]);
+  xor #(2) (p3, a[3], b[3]);
+
+  and #(2) (g0, a[0], b[0]);
+  and #(2) (g1, a[1], b[1]);
+  and #(2) (g2, a[2], b[2]);
+  and #(2) (g3, a[3], b[3]);
+
+  // Carry c1
+  wire c1_p0cin;
+  and #(2) (c1_p0cin, p0, cin);
+  or  #(2) (c1, g0, c1_p0cin);
+
+  // Carry c2
+  wire c2_p1g0;
+  wire c2_p1p0cin;
+
+  and #(2) (c2_p1g0, p1, g0);
+  and #(2) (c2_p1p0cin, p1, p0, cin);
+  or  #(2) (c2, g1, c2_p1g0, c2_p1p0cin);
+
+  // Carry c3
+  wire c3_p2g1;
+  wire c3_p2p1g0;
+  wire c3_p2p1p0cin;
+
+  and #(2) (c3_p2g1, p2, g1);
+  and #(2) (c3_p2p1g0, p2, p1, g0);
+  and #(2) (c3_p2p1p0cin, p2, p1, p0, cin);
+  or  #(2) (c3, g2, c3_p2g1, c3_p2p1g0, c3_p2p1p0cin);
+
+  // Carry c4 / cout
+  wire c4_p3g2;
+  wire c4_p3p2g1;
+  wire c4_p3p2p1g0;
+  wire c4_p3p2p1p0cin;
+
+  and #(2) (c4_p3g2, p3, g2);
+  and #(2) (c4_p3p2g1, p3, p2, g1);
+  and #(2) (c4_p3p2p1g0, p3, p2, p1, g0);
+  and #(2) (c4_p3p2p1p0cin, p3, p2, p1, p0, cin);
+
+  or #(2) (c4, g3, c4_p3g2, c4_p3p2g1,
+                c4_p3p2p1g0, c4_p3p2p1p0cin);
+
+  assign #(2) sum[0] = p0 ^ cin;
+  assign #(2) sum[1] = p1 ^ c1;
+  assign #(2) sum[2] = p2 ^ c2;
+  assign #(2) sum[3] = p3 ^ c3;
+
+  assign #(2) cout = c4;
+
 endmodule
